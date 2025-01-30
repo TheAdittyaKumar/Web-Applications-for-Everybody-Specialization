5df0663e2d6253f2a625eb386dfd4b6dbe4391c9
Adittya Kumar Chowdhury <155320738+TheAdittyaKumar@users.noreply.github.com>
2025-01-30 21:16:30 +0600

    Add files via upload

---
+++ Building web applications in php/php assignment/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ADITTYA KUMAR CHOWDHURY PHP</title>
+</head>
+<body>
+    <h1>ADITTYA KUMAR CHOWDHURY PHP</h1>
+    <p>The SHA256 hash of "ADITTYA KUMAR CHOWDHURY" is: 
+        <?php echo hash('sha256', 'ADITTYA KUMAR CHOWDHURY'); ?>
+    </p>
+    
+    <p>ASCII ART:</p>
+    <pre>
+        A        
+       A A       
+      A   A      
+     AAAAAAA     
+    A       A    
+   A         A   
+  A           A  
+    </pre>
+
+    <p><a href="fail.php">Click here to check the error setting</a></p>
+    <p><a href="check.php">Click here to cause a traceback</a></p>
+</body>
+</html>
